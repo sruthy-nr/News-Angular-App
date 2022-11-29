f7163e16774a2889290b3d4a6be74e986c0af858
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-29 16:54:53 +0530

    viewnews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ViewNewsComponent } from './view-news/view-news.component';
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewNewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-news/view-news.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-news',
+  templateUrl: './view-news.component.html',
+  styleUrls: ['./view-news.component.css']
+})
+export class ViewNewsComponent {
+  constructor(private api:ApiService){
+    api.fetchNews().subscribe(
+      (response)=>{
+      this.data=response;
+      }
+    )
+    
+  }
+  
+  data:any=[]
+}
+
